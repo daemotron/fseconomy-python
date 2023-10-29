9a781630f5dd0913eb87e24ca1286ae643073acc
daemotron <mail@daemotron.net>
2023-10-29 22:21:19 +0100

    improve inline documentation

--- src/fseconomy/util/hex.py
+++ src/fseconomy/util/hex.py
@@ -10,7 +10,9 @@ def is_hex(value: str) -> bool:
     """validate if a string represents a number in hexadecimal format
 
     :param value: string to validate
+    :type value: str
     :return: True if string represents a hexadecimal number, otherwise False
+    :rtype: bool
     """
     try:
         _ = int(value, 16)
