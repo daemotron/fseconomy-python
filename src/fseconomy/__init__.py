f8de43426cdc6a7e84884c07641866a9213c648c
daemotron <mail@daemotron.net>
2023-10-29 11:10:47 +0100

    add doc strings

--- src/fseconomy/__init__.py
+++ src/fseconomy/__init__.py
@@ -5,15 +5,27 @@ from util.hex import is_hex
 
 
 def set_service_key(key: Optional[str] = None):
+    """set the FSEconomy service key
+
+    :param key: string representing a valid FSEconomy service key
+    """
     if keys.validate_key(key):
         keys.SERVICE_KEY = key
 
 
 def set_access_key(key: Optional[str] = None):
+    """set the FSEconomy read access key
+
+    :param key: string representing a valid FSEconomy access key
+    """
     if keys.validate_key(key):
         keys.ACCESS_KEY = key
 
 
 def set_user_key(key: Optional[str] = None):
+    """set the FSEconomy user key
+
+    :param key: string representing a valid FSEconomy user key
+    """
     if keys.validate_key(key):
         keys.USER_KEY = key
