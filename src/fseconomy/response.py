793d68a1616419a714a1bbfb8593b2ca3b7d13b5
daemotron <mail@daemotron.net>
2023-11-18 12:46:08 +0100

    simplify response object

--- src/fseconomy/response.py
+++ src/fseconomy/response.py
@@ -13,28 +13,16 @@ class Response:
 
     :param status: HTTP status code sent by the server
     :type status: int
-    :param maintenance: flag signalling that server is in maintenance mode
-    :type maintenance: bool
-    :param ok: flag signalling a normal request flow with the server
-    :type ok: bool
-    :param fail: flag signalling an abnormal request flow with the server
-    :type fail: bool
     :param data: data received from the server represented by native Python data types
-    :param raw: raw data string received from the server
-    :type raw: str
-    :param message: message text detailing the status flags
-    :type message: str
+    :type data: Any
+    :param raw: raw data received from the server
+    :type raw: Union[str, bytes]
     """
 
-    def __init__(self, status: int = 0, maintenance: bool = False, ok: bool = False, fail: bool = False,
-                 data: Optional[Any] = None, raw: Union[str, bytes] = '', message: str = ''):
+    def __init__(self, status: int = 0, data: Optional[Any] = None, raw: Optional[Union[str, bytes]] = None):
         self._status: int = status
-        self._maintenance: bool = maintenance
-        self._ok: bool = ok
-        self._fail: bool = fail
         self._data: Any = data
-        self._raw: Union[str, bytes] = raw
-        self._message: str = message
+        self._raw: Optional[Union[str, bytes]] = raw
 
     @property
     def status(self) -> int:
@@ -46,40 +34,6 @@ class Response:
         self._status = http.HTTPStatus(int(value))
 
     @property
-    def maintenance(self) -> bool:
-        """Server maintenance flag
-
-        Not used in default mode; instead, an :exc:`~fseconomy.exceptions.FseServerMaintenanceError` will be raised."""
-        return self._maintenance
-
-    @maintenance.setter
-    def maintenance(self, value: bool):
-        self._maintenance = bool(value)
-
-    @property
-    def ok(self) -> bool:
-        """Request success flag
-
-        Always set to ``True`` in default mode. Any abnormal situation will be signalled by raising the corresponding
-        exception."""
-        return self._ok
-
-    @ok.setter
-    def ok(self, value: bool):
-        self._ok = bool(value)
-
-    @property
-    def fail(self) -> bool:
-        """Request failure flag
-
-        Not used in default mode; instead, an :exc:`~fseconomy.exceptions.FseServerRequestError` will be raised."""
-        return self._fail
-
-    @fail.setter
-    def fail(self, value: bool):
-        self._fail = bool(value)
-
-    @property
     def data(self) -> Optional[Any]:
         """Data received from the server decoded into native Python data types"""
         return self._data
@@ -89,24 +43,15 @@ class Response:
         self._data = value
 
     @property
-    def raw(self) -> Union[str, bytes]:
+    def raw(self) -> Optional[Union[str, bytes]]:
         """Data received from the server as raw string or bytes"""
         return self._raw
 
     @raw.setter
-    def raw(self, value: Union[str, bytes]):
+    def raw(self, value: Optional[Union[str, bytes]] = None):
         self._raw = value
 
     @property
     def binary(self) -> bool:
         """Raw data is binary"""
         return isinstance(self._raw, bytes)
-
-    @property
-    def message(self) -> str:
-        """Message with details regarding the set flags"""
-        return self._message
-
-    @message.setter
-    def message(self, value: str):
-        self._message = str(value)
