dc5b09b0accbcebb17d69757274f311e77f035d4
daemotron <mail@daemotron.net>
2025-08-08 18:07:40 +0200

    bump version to 0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'FSEconomy for Python'
 copyright = '2023–2025, Jesco "daemotron" Freund'
 author = 'Jesco "daemotron" Freund'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
